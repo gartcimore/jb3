1989d193c60fd851a171015d611ca68da73a4a1c
devnewton <devnewton@bci.im>
2014-12-04 12:26:55 +0100

    add post id norloge parsing

--- src/main/java/im/bci/jb3/logic/Norloge.java
+++ src/main/java/im/bci/jb3/logic/Norloge.java
@@ -2,6 +2,8 @@ package im.bci.jb3.logic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.joda.time.MutableDateTime;
@@ -11,9 +13,6 @@ import org.joda.time.format.DateTimeFormatterBuilder;
 import org.joda.time.format.DateTimeParser;
 
 public class Norloge {
-
-    private static final DateTimeFormatter norlogeParser;
-    private static final DateTimeFormatter norlogeFormatter = DateTimeFormat.forPattern("yyyy/MM/dd#hh:mm:ss");
 
     private String id;
     private DateTime time;
@@ -43,6 +42,10 @@ public class Norloge {
         this.bouchot = bouchot;
     }
 
+    private static final DateTimeFormatter norlogeParser;
+    private static final DateTimeFormatter norlogeFormatter = DateTimeFormat.forPattern("yyyy/MM/dd#HH:mm:ss");
+    private static final Pattern idPattern = Pattern.compile("#(\\w)*(?!\\w*:)");
+
     static {
         final DateTimeParser norlogeYearParser = new DateTimeFormatterBuilder().appendYear(2, 4).appendLiteral('/').toParser();
         final DateTimeParser norlogeMonthDayParser = new DateTimeFormatterBuilder().appendMonthOfYear(2).appendLiteral('/').appendDayOfMonth(2).appendLiteral('#').toParser();
@@ -54,11 +57,22 @@ public class Norloge {
 
     public static List<Norloge> parseNorloges(String message) {
         List<Norloge> result = new ArrayList<Norloge>();
+        Matcher matcher = idPattern.matcher(message);
+        while (matcher.find()) {
+            Norloge norloge = new Norloge();
+            norloge.setId(matcher.group(0).replace("#", ""));
+            result.add(norloge);
+        }
         int position = 0;
         while (position < message.length()) {
             MutableDateTime norlogeTime = new MutableDateTime();
             norlogeTime.setRounding(norlogeTime.getChronology().secondOfMinute());
-            int newPosition = norlogeParser.parseInto(norlogeTime, message, position);
+            int newPosition;
+            try {
+                newPosition = norlogeParser.parseInto(norlogeTime, message, position);
+            } catch (Exception e) {
+                newPosition = -1;
+            }
             if (newPosition >= 0) {
                 Norloge norloge = new Norloge();
                 norloge.setTime(norlogeTime.toDateTime());
@@ -74,21 +88,22 @@ public class Norloge {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        if(StringUtils.isNotBlank(id)) {
+        if (StringUtils.isNotBlank(id)) {
             sb.append('#');
             sb.append(id);
         }
-        if(null != time) {
+        if (null != time) {
             sb.append(norlogeFormatter.print(time));
         }
-        if(StringUtils.isNotBlank(bouchot)) {
+        if (StringUtils.isNotBlank(bouchot)) {
             sb.append('@').append(bouchot);
         }
         return sb.toString();
     }
 
     public static void main(String[] args) throws Exception {
-        for (Norloge norloge : parseNorloges("moules< 16:25:11 les norloges du style 12/02#18:28:15 ou 2012/01/02#18:12:12 c'est conforme à la rfc ?")) {
+        final String post = "moules< 16:25:11 les norloges du style 12/02#18:28:15 ou 2012/01/02#18:12:12 c'est conforme à la rfc #22:30 #tamaman #1234 #n3 ?";
+        for (Norloge norloge : parseNorloges(post)) {
             System.out.println(norloge);
         }
     }
