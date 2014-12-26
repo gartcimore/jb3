44d3f869d7b560d3df3e371f5dcd29550265330a
devnewton <>
2014-12-26 19:26:13 +0100

    remove useless imports

--- src/main/java/im/bci/jb3/controllers/ArchiveController.java
+++ src/main/java/im/bci/jb3/controllers/ArchiveController.java
@@ -1,15 +1,11 @@
 package im.bci.jb3.controllers;
 
-import im.bci.jb3.data.Fortune;
-import im.bci.jb3.data.FortuneRepository;
 import im.bci.jb3.data.PostRepository;
-import im.bci.jb3.frontend.FortuneSearchFO;
 import im.bci.jb3.frontend.PostSearchRQ;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
