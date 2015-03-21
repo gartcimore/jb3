3b518e8f1c10df8dc33d163f05e1d8c8a44c0941
devnewton <>
2015-03-21 15:03:43 +0100

    secure and unsafe totoz

--- src/main/resources/static/jb3-common.js
+++ src/main/resources/static/jb3-common.js
@@ -3,7 +3,7 @@ jb3_common = {
         var formattedMessage = message.replace(/(\s|^)#(\w+)/g, '$1<span class="jb3-cite" data-ref="$2">#$2</span>');
         formattedMessage = formattedMessage.replace(/(\s|^)(https?:\/\/\S+)/gi, '$1<a href="$2" target="_blank" rel="nofollow">[url]</a>');
         formattedMessage = formattedMessage.replace(/(\s|^)(ftp:\/\/\S+)/gi, '$1<a href="$2" target="_blank" rel="nofollow">[url]</a>');
-        formattedMessage = formattedMessage.replace(/\[\:([^\t\)\]]+)\]/g, '<a class="jb3-totoz">[:$1]<img src="http://sfw.totoz.eu/gif/$1.gif"/></a>');
+        formattedMessage = formattedMessage.replace(/\[\:([^\t\)\]]+)\]/g, '<a class="jb3-totoz">[:$1]<img src="https://nsfw.totoz.eu/gif/$1.gif"/></a>');
         formattedMessage = formattedMessage.replace(/(\s|^)([a-zA-Z0-9-_]*)&lt;(\s|$)/g, '$1<span class="jb3-bigorno">$2&lt;</span>');
         return formattedMessage;
     },
