2cfe20ec2302f39e73c8f08bc2fb0bc22944f4e2
devnewton <devnewton@bci.im>
2015-04-16 11:08:18 +0200

    avoid multiple stomp.js import

--- src/main/resources/static/webdirectcoin.js
+++ src/main/resources/static/webdirectcoin.js
@@ -4,30 +4,34 @@ function Webdirectcoin() {
 Webdirectcoin.prototype.initWebsocket = function (url) {
     if (typeof WebSocket === "function") {
         importScripts("/webjars/stomp-websocket/2.3.1-1/stomp.js");
-        var self = this;
-        var stompClient = Stomp.client(url);
-        stompClient.connect({}, function (frame) {
-            console.log('WebDirectCoin connected: ' + frame);
-            stompClient.subscribe('/topic/posts', function (postsMessage) {
-                postMessage({type: "posts", posts: JSON.parse(postsMessage.body)});
-            });
-            stompClient.subscribe('/topic/debug', function () {
-            });
-            self.stompClient = stompClient;
-            setInterval(function () {
-                self.stompClient.send("/webdirectcoin/presence", {}, "plop");
-            }, 20000);
-            postMessage({type: "connected"});
-        }, function (error) {
-            console.log('WebDirectCoin error: ' + error + "\nTry to reconnect...");
-            setTimeout(function () {
-                self.initWebsocket(url);
-            }, 30000);
-        }
-        );
+        this.connectWebsocket(url);
     } else {
         postMessage({type: "webdirectcoin_not_available"});
     }
+};
+
+Webdirectcoin.prototype.connectWebsocket = function (url) {
+    var self = this;
+    var stompClient = Stomp.client(url);
+    stompClient.connect({}, function (frame) {
+        console.log('WebDirectCoin connected: ' + frame);
+        stompClient.subscribe('/topic/posts', function (postsMessage) {
+            postMessage({type: "posts", posts: JSON.parse(postsMessage.body)});
+        });
+        stompClient.subscribe('/topic/debug', function () {
+        });
+        self.stompClient = stompClient;
+        setInterval(function () {
+            self.stompClient.send("/webdirectcoin/presence", {}, "plop");
+        }, 20000);
+        postMessage({type: "connected"});
+    }, function (error) {
+        console.log('WebDirectCoin error: ' + error + "\nTry to reconnect...");
+        setTimeout(function () {
+            self.connectWebsocket(url);
+        }, 30000);
+    }
+    );
 };
 
 Webdirectcoin.prototype.onmessage = function (event) {
