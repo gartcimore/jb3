fb57010d53c50094ada2a432ce6caa203d4ed099
devnewton <devnewton@bci.im>
2016-06-14 23:37:16 +0200

    Update wro4j configuration so Eclipse can update static resources

--- src/main/resources/static/jb3.js
+++ src/main/resources/static/jb3.js
@@ -312,6 +312,7 @@ jb3 = {
         control.value = control.value.substring(0, control.selectionStart) + text + control.value.substr(control.selectionEnd);
         control.focus();
         control.setSelectionRange(selectionEnd, selectionEnd);
-    }
+    },
+    hello: "pouetworld"
 };
 jb3.init();
