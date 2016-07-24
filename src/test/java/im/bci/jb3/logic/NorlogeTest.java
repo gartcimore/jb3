e44599d0a9167113e072965a9e8ca7f6e64bb458
devnewton <devnewton@bci.im>
2016-07-24 10:32:40 +0200

    fix unit test

--- src/test/java/im/bci/jb3/logic/NorlogeTest.java
+++ src/test/java/im/bci/jb3/logic/NorlogeTest.java
@@ -17,15 +17,15 @@ public class NorlogeTest {
         System.out.println("parseNorloges");
         String message = " moules< 16:25:11 les posts du style 12/31#18:28:15 ou 2012/01/02#18:12:12 c'est conforme à la rfc et #tamaman #1234 #n3 #lol@dlfp  #troll@euro.net? et 12:30?";
         List<Norloge> expResult = new ArrayList<Norloge>();
-        expResult.add(new Norloge().withTime(new DateTime().withZone(LegacyUtils.legacyTimeZone).withHourOfDay(16).withMinuteOfHour(25).withSecondOfMinute(11).withMillisOfSecond(0)));
-        expResult.add(new Norloge().withTime(new DateTime().withZone(LegacyUtils.legacyTimeZone).withMonthOfYear(12).withDayOfMonth(31).withHourOfDay(18).withMinuteOfHour(28).withSecondOfMinute(15).withMillisOfSecond(0)));
-        expResult.add(new Norloge().withTime(new DateTime().withZone(LegacyUtils.legacyTimeZone).withYear(2012).withMonthOfYear(1).withDayOfMonth(2).withHourOfDay(18).withMinuteOfHour(12).withSecondOfMinute(12).withMillisOfSecond(0)));
+        expResult.add(new Norloge().withTime(DateTime.now(LegacyUtils.legacyTimeZone).withHourOfDay(16).withMinuteOfHour(25).withSecondOfMinute(11).withMillisOfSecond(0)));
+        expResult.add(new Norloge().withTime(DateTime.now(LegacyUtils.legacyTimeZone).withMonthOfYear(12).withDayOfMonth(31).withHourOfDay(18).withMinuteOfHour(28).withSecondOfMinute(15).withMillisOfSecond(0)));
+        expResult.add(new Norloge().withTime(DateTime.now(LegacyUtils.legacyTimeZone).withYear(2012).withMonthOfYear(1).withDayOfMonth(2).withHourOfDay(18).withMinuteOfHour(12).withSecondOfMinute(12).withMillisOfSecond(0)));
         expResult.add(new Norloge().withId("tamaman"));
         expResult.add(new Norloge().withId("1234"));
         expResult.add(new Norloge().withId("n3"));
         expResult.add(new Norloge().withId("lol").withBouchot("dlfp"));
         expResult.add(new Norloge().withId("troll").withBouchot("euro.net"));
-        expResult.add(new Norloge().withTime(new DateTime().withZone(LegacyUtils.legacyTimeZone).withHourOfDay(12).withMinuteOfHour(30).withSecondOfMinute(0).withMillisOfSecond(0)));
+        expResult.add(new Norloge().withTime(DateTime.now(LegacyUtils.legacyTimeZone).withHourOfDay(12).withMinuteOfHour(30).withSecondOfMinute(0).withMillisOfSecond(0)));
         List<Norloge> result = Norloge.parseNorloges(message);
         assertArrayEquals(expResult.toArray(), result.toArray());
     }
