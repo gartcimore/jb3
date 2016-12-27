22f37ea78efc136aa0eb2548e4e489f47ef94702
devnewton <devnewton@bci.im>
2016-12-27 14:02:09 +0100

    add audio record paste

--- src/main/resources/static/assets/tags/jb3-paste-form--tag.js
+++ src/main/resources/static/assets/tags/jb3-paste-form--tag.js
@@ -7,6 +7,7 @@ riot
 			    <div class="c-tab-heading { \'c-tab-heading--active\': selectedTab == \'text\' }" data-tab="text" onclick="{ selectTab }">Text</div>\
 			    <div class="c-tab-heading { \'c-tab-heading--active\': selectedTab == \'image\' }" data-tab="image" onclick="{ selectTab }">Image</div>\
 			    <div class="c-tab-heading { \'c-tab-heading--active\': selectedTab == \'sketch\' }" data-tab="sketch" onclick="{ selectTab }">Sketch</div>\
+                            <div class="c-tab-heading { \'c-tab-heading--active\': selectedTab == \'record\' }" data-tab="record" onclick="{ selectTab }">Record</div>\
 			    <div class="c-tab-heading { \'c-tab-heading--active\': selectedTab == \'file\' }" data-tab="file" onclick="{ selectTab }">File</div>\
 		     </div>\
 			<div class="c-tabs__tab { \'c-tabs__tab--active\': selectedTab == \'text\' }">\
@@ -76,6 +77,31 @@ riot
 				  </div>\
 				</div>\
 			</div>\
+                        <div class="c-tabs__tab { \'c-tabs__tab--active\': selectedTab == \'record\' }">\
+                            <div class="o-form-element">\
+                                <button class="c-button" onclick="{ startRecord }">Record</button>\
+                                <button class="c-button" onclick="{ stopRecord }">Stop</button>\
+                            </div>\
+                            <div class="o-form-element">\
+                                <audio name="recordedAudio" controls></audio>\
+                            </div>\
+                            <div class="o-form-element">\
+                                <button class="c-button c-button--info" onclick="{ uploadRecord }" >Upload</button>\
+                                <progress name="recordProgress" value="0" max="100"></progress>\
+                            </div>\
+                            <div if="{ pastedRecordUrl }" class="c-card  c-card--success">\
+				  <div class="c-card__item c-card__item--divider">Pasted!</div>\
+				  <div class="c-card__item">\
+				    <p class="c-paragraph"><a class="c-link  jb3-pasted-url" href="{ pastedRecordUrl }">{ pastedRecordUrl }</a></p>\
+				  </div>\
+                            </div>\
+                            <div if="{ pastedRecordError }" class="c-card  c-card--error">\
+                              <div class="c-card__item c-card__item--divider">Error :-(</div>\
+                              <div class="c-card__item">\
+                                <p class="c-paragraph">{ pastedRecordError }</p>\
+                              </div>\
+                            </div>\
+                        </div>\
 			<div class="c-tabs__tab { \'c-tabs__tab--active\': selectedTab == \'file\' }">\
 				<form name="pasteFileForm" class="c-fieldset" action="/api/paste/file" method="post">\
 					<div class="o-form-element">\
@@ -99,7 +125,7 @@ riot
 			</div>\
 		</div>\
 ',
-'.jb3-paste-sketch-tools {\
+                '.jb3-paste-sketch-tools {\
     margin-bottom: 10px;\
 }\
 .jb3-paste-sketch-container {\
@@ -113,54 +139,114 @@ riot
 }\
 ',
                 function(opts) {
-                var self = this;
-                        this.selectedTab = 'text';
+                    var self = this;
+                    this.selectedTab = 'text';
+                        this.startRecord = function() {
+                            this.cancelRecord();
+                            var constraints = { audio: true, video: false }; 
+                            navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+                                self.mediaRecorder = new MediaRecorder(mediaStream);
+                                self.recordedChunks = [];
+                                self.mediaRecorder.ondataavailable = function(e) {
+                                    self.recordedChunks.push(e.data);
+                                }
+                                self.mediaRecorder.start();
+                            },function(){
+                                console.log('prout');
+                            })
+                            .catch(function(err) { 
+                                console.log(err.name + ": " + err.message);
+                            });
+                        };
+                        this.cancelRecord = function() {
+                            if(self.mediaRecorder) {
+                                self.mediaRecorder.onstop = null;
+                                self.mediaRecorder.stop();
+                                self.recordedChunks = [];
+                            }
+                        };
+                        this.stopRecord = function() {
+                            if(self.mediaRecorder) {
+                                self.mediaRecorder.onstop = function() {
+                                    self.recordedBlob = new Blob(self.recordedChunks, { 'type' : 'audio/ogg; codecs=opus' });
+                                    self.recordedChunks = [];
+                                    self.recordedAudio.src = window.URL.createObjectURL(self.recordedBlob);
+                                }
+                                self.mediaRecorder.stop();
+                            }
+                        };
+                        this.uploadRecord = function() {
+                            if(self.recordedBlob) {
+                                var formData = new FormData();
+                                formData.append("pfile", self.recordedBlob, "record.ogg");
+                                var xhr = new XMLHttpRequest;
+                                xhr.onprogress = function(e) {
+                                    var percentComplete = (e.loaded / e.total) * 100;
+                                    self.recordProgress.value = percentComplete;
+                                };
+                                xhr.onreadystatechange = function (event) {
+                                if (xhr.readyState == 4) {
+                                if (xhr.status == 200) {
+                                var data = JSON.parse(xhr.response);
+                                        self.pastedRecordError = null;
+                                        self.pastedRecordUrl = data.url;
+                                } else {
+                                self.pastedRecordError = 'Error during record upload';
+                                        self.pastedRecordUrl = null;
+                                }
+                                self.update();
+                                }
+                                };
+                                xhr.open("POST", "/api/paste/file");
+                                xhr.send(formData);
+                            }
+                        };
                         var sketchForm = $(self.pasteSketchForm);
                         this.sketchpad = new Sketchpad(this.sketchCanvasContainer, {
-                            width: 512,
-                            height: 384,
-                            line: {
+                        width: 512,
+                                height: 384,
+                                line: {
                                 color: '#000',
-                                size: 5
-                            }
+                                        size: 5
+                                }
                         });
                         this.undoSketch = function() {
-                            this.sketchpad.undo();
+                        this.sketchpad.undo();
                         };
                         this.redoSketch = function() {
-                            this.sketchpad.redo();
+                        this.sketchpad.redo();
                         };
                         this.changeSketchColor = function(e) {
-                            this.sketchpad.setLineColor(e.target.value);
+                        this.sketchpad.setLineColor(e.target.value);
                         };
                         this.changeSketchPenSize = function(e) {
-                            this.sketchpad.setLineSize(e.target.value);
+                        this.sketchpad.setLineSize(e.target.value);
                         };
                         this.uploadSketch = function() {
-                                this.sketchpad.canvas.toBlob(function(blob) {
-                                var formData = new FormData();
-                                        formData.append("pimage", blob, "sketch.png");
-                                        var xhr = new XMLHttpRequest;
-                                        xhr.onprogress = function(e) {
-                                            var percentComplete = (e.loaded / e.total) * 100;
-                                            sketchForm.find('progress').val(percentComplete);
-                                        };
-                                        xhr.onreadystatechange = function (event) {
-                                            if (xhr.readyState == 4) {
-                                                if (xhr.status == 200) {
-                                                    var data = JSON.parse(xhr.response);
-                                                    self.pastedSketchError = null;
-                                                    self.pastedSketchUrl = data.url;
-                                                } else {
-                                                    self.pastedSketchError = 'Error during image upload';
-                                                    self.pastedSketchUrl = null;
-                                                }
-                                                self.update();
-                                            }
-                                        };
-                                        xhr.open("POST", "/api/paste/image");
-                                        xhr.send(formData);
-                                }, "image/png");
+                        this.sketchpad.canvas.toBlob(function(blob) {
+                        var formData = new FormData();
+                                formData.append("pimage", blob, "sketch.png");
+                                var xhr = new XMLHttpRequest;
+                                xhr.onprogress = function(e) {
+                                var percentComplete = (e.loaded / e.total) * 100;
+                                        sketchForm.find('progress').val(percentComplete);
+                                };
+                                xhr.onreadystatechange = function (event) {
+                                if (xhr.readyState == 4) {
+                                if (xhr.status == 200) {
+                                var data = JSON.parse(xhr.response);
+                                        self.pastedSketchError = null;
+                                        self.pastedSketchUrl = data.url;
+                                } else {
+                                self.pastedSketchError = 'Error during image upload';
+                                        self.pastedSketchUrl = null;
+                                }
+                                self.update();
+                                }
+                                };
+                                xhr.open("POST", "/api/paste/image");
+                                xhr.send(formData);
+                        }, "image/png");
                         };
                         self.selectTab = function(e) {
                         self.selectedTab = e.target.dataset.tab
@@ -212,12 +298,12 @@ riot
                         maxWidth : 'auto',
                                 formSubmitted : function(xhr) {
                                 if (xhr.status == 200) {
-                                    var data = JSON.parse(xhr.response);
-                                    self.pastedImageError = null;
-                                    self.pastedImageUrl = data.url;
+                                var data = JSON.parse(xhr.response);
+                                        self.pastedImageError = null;
+                                        self.pastedImageUrl = data.url;
                                 } else {
-                                    self.pastedImageError = 'Error during image upload';
-                                    self.pastedImageUrl = null;
+                                self.pastedImageError = 'Error during image upload';
+                                        self.pastedImageUrl = null;
                                 }
                                 self.update();
                                 }
