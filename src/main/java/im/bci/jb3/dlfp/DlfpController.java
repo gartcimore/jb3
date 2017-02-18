d6ba327b8a97c73048aedb5695364dc2715d0944
devnewton <devnewton@bci.im>
2017-02-18 14:30:09 +0100

    make oauth with dlfp work

--- src/main/java/im/bci/jb3/dlfp/DlfpController.java
+++ src/main/java/im/bci/jb3/dlfp/DlfpController.java
@@ -2,8 +2,16 @@ package im.bci.jb3.dlfp;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
@@ -20,72 +28,109 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/dlfp")
 public class DlfpController {
 
-	@Value("${jb3.dlfp.oauth.client_id:}")
-	private String clientId;
+    @Value("${jb3.dlfp.oauth.client_id:}")
+    private String clientId;
 
-	@Value("${jb3.dlfp.oauth.client_secret:}")
-	private String clientSecret;
+    @Value("${jb3.dlfp.oauth.client_secret:}")
+    private String clientSecret;
 
-	@RequestMapping(path = "/connect", method = RequestMethod.GET)
-	public RedirectView connect(RedirectAttributes attributes) {
-		attributes.addAttribute("client_id", clientId);
-		attributes.addAttribute("redirect_uri", buildRedirectURI());
-		attributes.addAttribute("response_type", "code");
-		attributes.addAttribute("scope", "board");
-		return new RedirectView("https://linuxfr.org/api/oauth/authorize");
-	}
+    @RequestMapping(path = "/connect", method = RequestMethod.GET)
+    public RedirectView connect(RedirectAttributes attributes) {
+        attributes.addAttribute("client_id", clientId);
+        attributes.addAttribute("redirect_uri", buildRedirectURI());
+        attributes.addAttribute("response_type", "code");
+        attributes.addAttribute("scope", "board");
+        return new RedirectView("https://linuxfr.org/api/oauth/authorize");
+    }
 
-	public static class OauthToken {
-		private String access_token;
-		private String refresh_token;
-		private String expires_in;
+    public static class OauthToken {
 
-		public String getAccess_token() {
-			return access_token;
-		}
+        private String access_token;
+        private String refresh_token;
+        private String expires_in;
 
-		public void setAccess_token(String access_token) {
-			this.access_token = access_token;
-		}
+        public String getAccess_token() {
+            return access_token;
+        }
 
-		public String getRefresh_token() {
-			return refresh_token;
-		}
+        public void setAccess_token(String access_token) {
+            this.access_token = access_token;
+        }
 
-		public void setRefresh_token(String refresh_token) {
-			this.refresh_token = refresh_token;
-		}
+        public String getRefresh_token() {
+            return refresh_token;
+        }
 
-		public String getExpires_in() {
-			return expires_in;
-		}
+        public void setRefresh_token(String refresh_token) {
+            this.refresh_token = refresh_token;
+        }
 
-		public void setExpires_in(String expires_in) {
-			this.expires_in = expires_in;
-		}
-	}
+        public String getExpires_in() {
+            return expires_in;
+        }
 
-	@RequestMapping(path = "/connected", method = RequestMethod.GET)
-	public String connect(Model model, @RequestParam String code) {
-		RestTemplate restTemplate = new RestTemplate();
-		try {
-			MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-			map.add("client_id", clientId);
-			map.add("client_secret", clientSecret);
-			map.add("code", code);
-			map.add("grant_type", "authorization_code");
-			map.add("redirect_uri", buildRedirectURI());
-			OauthToken token = restTemplate.postForObject("https://linuxfr.org/api/oauth/token", map, OauthToken.class);
-			model.addAttribute("token", token);
-		} catch (Exception ex) {
-			Logger.getLogger(DlfpController.class.getName()).log(Level.WARNING, null, ex);
-		}
-		model.addAttribute("wro-group", "dlfp");
-		return "dlfp/connected";
-	}
+        public void setExpires_in(String expires_in) {
+            this.expires_in = expires_in;
+        }
+    }
 
-	private String buildRedirectURI() {
-		return ServletUriComponentsBuilder.fromCurrentRequest().replacePath("/dlfp/connected").build().toString();
-	}
+    @RequestMapping(path = "/connected", method = RequestMethod.GET)
+    public String connect(Model model, @RequestParam String code) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+            headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+            requestBody.add("client_id", clientId);
+            requestBody.add("client_secret", clientSecret);
+            requestBody.add("code", code);
+            requestBody.add("grant_type", "authorization_code");
+            requestBody.add("redirect_uri", buildRedirectURI());
+
+            HttpEntity formEntity = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<OauthToken> response
+                    = restTemplate.exchange("https://linuxfr.org/api/oauth/token", HttpMethod.POST,
+                            formEntity, OauthToken.class);
+            model.addAttribute("token", response.getBody());
+        } catch (Exception ex) {
+            Logger.getLogger(DlfpController.class.getName()).log(Level.WARNING, null, ex);
+        }
+        model.addAttribute("wro-group", "dlfp");
+        return "dlfp/connected";
+    }
+
+        @RequestMapping(path = "/refresh-token", method = RequestMethod.GET)
+    public String refreshToken(Model model, @RequestParam String token) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+            headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+            requestBody.add("client_id", clientId);
+            requestBody.add("client_secret", clientSecret);
+            requestBody.add("refresh_token", token);
+            requestBody.add("grant_type", "refresh_token");
+
+            HttpEntity formEntity = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<OauthToken> response
+                    = restTemplate.exchange("https://linuxfr.org/api/oauth/token", HttpMethod.POST,
+                            formEntity, OauthToken.class);
+            model.addAttribute("token", response.getBody());
+        } catch (Exception ex) {
+            Logger.getLogger(DlfpController.class.getName()).log(Level.WARNING, null, ex);
+        }
+        model.addAttribute("wro-group", "dlfp");
+        return "dlfp/connected";
+    }
+    
+    private String buildRedirectURI() {
+        return ServletUriComponentsBuilder.fromCurrentRequest().replacePath("/dlfp/connected").replaceQuery("").build().toString();
+    }
 
 }
