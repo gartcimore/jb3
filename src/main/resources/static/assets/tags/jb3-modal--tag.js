588fd3b9d320d12d6bafdb76c95f7b15379581cd
devnewton <devnewton@bci.im>
2017-03-05 21:51:08 +0100

    remove paste modal header

--- src/main/resources/static/assets/tags/jb3-modal--tag.js
+++ src/main/resources/static/assets/tags/jb3-modal--tag.js
@@ -1,35 +1,47 @@
+function jb3ModalConstructor(opts) {
+    var self = this;
+    this.show = function () {
+        this.opts.visible = true;
+        this.update();
+    };
+    this.hide = function () {
+        self.opts.visible = false;
+        self.update();
+    };
+}
+
+var jb3ModalTemplate = '\
+<div class="c-overlay" if="{ opts.visible }" onclick="{ hide }"></div>\
+<div class="o-modal o-modal--full"  if="{ opts.visible }">\
+    <div class="c-card">\
+        <header class="c-card__header" if="{ opts.showHeader }">\
+            <button type="button" class="c-button c-button--close" onclick="{ hide }">×</button>\
+            <h3 class="c-heading c-heading--small">{ opts.title }</h3>\
+        </header>\
+        <div class="{\'jb3-modal-headerless-body\':!opts.showHeader, \'c-card__body\':true, \'o-panel\':true }">\
+              <yield/>\
+        </div>\
+        <footer class="c-card__footer  c-card__footer--block">\
+            <div class="c-input-group">\
+                <button each="{ opts.buttons }" type="button" class="c-button c-button--block { \'c-button--\' + type }" onclick="{ action }" style="{ style }">\
+                    { text }\
+                </button>\
+            </div>\
+        </footer>\
+    </div>\
+</div>\
+';
+
+var jb3ModalStyles = '\
+	.jb3-modal-headerless-body {\
+		top: 0 !important;\
+                bottom: 2.5em !important;\
+	}\
+';
+
 riot
         .tag(
                 'jb3-modal',
-                '\
-				<div class="c-overlay" if="{ opts.visible }" onclick="{ hide }"></div>\
-				<div class="o-modal o-modal--full"  if="{ opts.visible }">\
-                                    <div class="c-card">\
-                                        <header class="c-card__header">\
-                                            <button type="button" class="c-button c-button--close" onclick="{ hide }">×</button>\
-                                            <h3 class="c-heading c-heading--small">{ opts.title }</h3>\
-                                        </header>\
-                                        <div class="c-card__body o-panel">\
-                                              <yield/>\
-                                        </div>\
-                                        <footer class="c-card__footer  c-card__footer--block">\
-                                            <div class="c-input-group">\
-                                                <button each="{ opts.buttons }" type="button" class="c-button c-button--block { \'c-button--\' + type }" onclick="{ action }" style="{ style }">\
-                                                    { text }\
-                                                </button>\
-                                            </div>\
-				 	</footer>\
-                                    </div>\
-				</div>\
-',
-                function (opts) {
-                    var self = this;
-                    this.show = function () {
-                        this.opts.visible = true;
-                        this.update();
-                    };
-                    this.hide = function () {
-                        self.opts.visible = false;
-                        self.update();
-                    }
-                });
+                jb3ModalTemplate,
+                jb3ModalStyles,
+                jb3ModalConstructor);
