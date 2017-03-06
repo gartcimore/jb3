101bd9e54bb4df865d2c381347fd1d2cbb493acd
devnewton <devnewton@bci.im>
2017-03-06 15:16:17 +0100

    code clean

--- src/main/resources/static/assets/tags/jb3-revisions-modal--tag.js
+++ src/main/resources/static/assets/tags/jb3-revisions-modal--tag.js
@@ -1,30 +1,27 @@
-riot
-        .tag(
-                'jb3-revisions-modal',
-                '\
-				<div class="c-overlay" if="{ opts.visible }" onclick="{ hide }"></div>\
-				<div class="o-modal"  if="{ opts.visible }" style="height: 80%; overflow: scroll">\
-				  <header class="o-modal__header">\
-				    <button type="button" class="c-button c-button--close" onclick="{ hide }">×</button>\
-				    <h3 class="c-heading c-heading--small">{ opts.title }</h3>\
-				  </header>\
-				  <div class="o-modal__body">\
-				  	<jb3-raw content="{ opts.revisions }"></jb3-raw>\
-				  </div>\
-				</div>\
-',
-                function (opts) {
-                    var self = this;
-                    //self.raw = self.tags['jb3-raw'];
-                    this.on('show', function (revisions) {
-                        this.opts.revisions = revisions;
-                        this.opts.visible = true;
-                        this.update();
-                        //  self.raw.content = revisions;
-                        //self.raw.update();
-                    });
-                    this.hide = function () {
-                        self.opts.visible = false;
-                        self.update();
-                    };
-                });
+function jb3RevisionsModalConstructor(opts) {
+    var self = this;
+    this.on('show', function (revisions) {
+        this.opts.revisions = revisions;
+        this.opts.visible = true;
+        this.update();
+    });
+    this.hide = function () {
+        self.opts.visible = false;
+        self.update();
+    };
+}
+
+var jb3RevisionsModalTemplate = '\
+<div class="c-overlay" if="{ opts.visible }" onclick="{ hide }"></div>\
+<div class="o-modal"  if="{ opts.visible }" style="height: 80%; overflow: scroll">\
+    <header class="o-modal__header">\
+        <button type="button" class="c-button c-button--close" onclick="{ hide }">×</button>\
+        <h3 class="c-heading c-heading--small">{ opts.title }</h3>\
+    </header>\
+    <div class="o-modal__body">\
+        <jb3-raw content="{ opts.revisions }"></jb3-raw>\
+    </div>\
+</div>\
+';
+
+riot.tag('jb3-revisions-modal', jb3RevisionsModalTemplate, jb3RevisionsModalConstructor);
