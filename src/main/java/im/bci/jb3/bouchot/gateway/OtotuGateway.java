06570f9ec2f407377fffcfffec3b00acec6eb32c
devnewton <devnewton@bci.im>
2017-07-07 18:46:54 +0200

    Add ototu gateway #33

---
+++ src/main/java/im/bci/jb3/bouchot/gateway/OtotuGateway.java
@@ -0,0 +1,28 @@
+package im.bci.jb3.bouchot.gateway;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author devnewton <devnewton@bci.im>
+ */
+@Component
+public class OtotuGateway extends AbstractTsvBouchotGateway {
+
+    private static BouchotConfig createConf() {
+        BouchotConfig conf = new BouchotConfig();
+        conf.setRoom("ototu");
+        conf.setGetUrl("https://ototu.euromussels.eu/goboard/backend/tsv");
+        conf.setPostUrl("https://ototu.euromussels.eu/goboard/post");
+        conf.setMessageContentParameterName("message");
+        conf.setTagsEncoded(false);
+        conf.setUsingXPost(false);
+        conf.setLastIdParameterName("last");
+        return conf;
+    }
+
+    public OtotuGateway() {
+        super(createConf());
+    }
+
+}
