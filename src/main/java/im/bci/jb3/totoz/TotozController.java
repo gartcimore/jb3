a8430325e435404e546fa3031cccea88933dc5ac
devnewton <devnewton@bci.im>
2018-03-06 17:36:55 +0100

    code clean

--- src/main/java/im/bci/jb3/totoz/TotozController.java
+++ src/main/java/im/bci/jb3/totoz/TotozController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
