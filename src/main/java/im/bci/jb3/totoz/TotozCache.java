3ac812681cd8de68b5813190cef5f66cd8830e6a
devnewton <devnewton@bci.im>
2019-04-24 22:35:53 +0200

    fork doesnt know how to do a simple json

--- src/main/java/im/bci/jb3/totoz/TotozCache.java
+++ src/main/java/im/bci/jb3/totoz/TotozCache.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.net.URL;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -19,7 +16,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Component
 public class TotozCache {
 
-    private final Log LOGGER = LogFactory.getLog(this.getClass());
     private File totozDir;
     
     @Value("${jb3.totoz.url}")
@@ -36,19 +32,6 @@ public class TotozCache {
             this.totozDir.mkdirs();
         } else {
             this.totozDir = new File(totozDir);
-        }
-    }
-
-    @Async("mouleExecutor")
-    public void saveTotozMetaData(String totoz) {
-        try {
-            File totozMetadataFile = new File(totozDir, totoz + ".json");
-            if (!totozMetadataFile.exists()) {
-                URL totozMetadataUrl = UriComponentsBuilder.fromHttpUrl(totozUrl).path("/totoz/").path(totoz).path("/info.json").build().toUri().toURL();
-                FileUtils.copyURLToFile(totozMetadataUrl, totozMetadataFile);
-            }
-        } catch (Exception e) {
-            LOGGER.warn("Cannot save totoz metadata for " + totoz, e);
         }
     }
 
--- src/main/java/im/bci/jb3/totoz/TotozController.java
+++ src/main/java/im/bci/jb3/totoz/TotozController.java
@@ -3,50 +3,41 @@ package im.bci.jb3.totoz;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @RequestMapping("/totoz")
-public class TotozController  {
+public class TotozController {
 
-    @Autowired
-    private TotozCache cache;
-    
-    @RequestMapping("/img/{totoz}")
-    @ResponseBody
-    public ResponseEntity<FileSystemResource> img(@PathVariable("totoz") String totoz)
-            throws MalformedURLException, IOException {
-        File totozFile = cache.cacheTotoz(totoz);
-        cache.saveTotozMetaData(totoz);
-        return ResponseEntity.ok().lastModified(totozFile.lastModified()).contentType(detectContentType(totozFile))
-                .contentLength(totozFile.length()).body(new FileSystemResource(totozFile));
-    }
+	@Autowired
+	private TotozCache cache;
 
-    private MediaType detectContentType(File totozFile) {
-        try {
-            return MediaType.parseMediaType(Files.probeContentType(totozFile.toPath()));
-        } catch (Exception e) {
-            return MediaType.ALL;
-        }
+	@RequestMapping("/img/{totoz}")
+	@ResponseBody
+	public ResponseEntity<FileSystemResource> img(@PathVariable("totoz") String totoz)
+			throws MalformedURLException, IOException {
+		File totozFile = cache.cacheTotoz(totoz);
+		return ResponseEntity.ok().lastModified(totozFile.lastModified()).contentType(detectContentType(totozFile))
+				.contentLength(totozFile.length()).body(new FileSystemResource(totozFile));
+	}
 
-    }
+	private MediaType detectContentType(File totozFile) {
+		try {
+			return MediaType.parseMediaType(Files.probeContentType(totozFile.toPath()));
+		} catch (Exception e) {
+			return MediaType.ALL;
+		}
+
+	}
 
 }
