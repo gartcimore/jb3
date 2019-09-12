3bd204efacd0b1bc9bf5027dfbc4f69099896bb3
devnewton <devnewton@bci.im>
2019-09-12 15:19:17 +0200

    use StandardCharsets.UTF_8

--- src/main/java/im/bci/jb3/handlebars/RawIncludeHelper.java
+++ src/main/java/im/bci/jb3/handlebars/RawIncludeHelper.java
@@ -5,6 +5,7 @@ import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.stereotype.Component;
 
@@ -15,11 +16,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class RawIncludeHelper implements Helper<String> {
 
-    private static final Charset CHARSET = Charset.forName("UTF-8");
-
 	@Override
     public CharSequence apply(String file, Options options) throws IOException {
-        return new Handlebars.SafeString(options.handlebars.getLoader().sourceAt(file).content(CHARSET));
+        return new Handlebars.SafeString(options.handlebars.getLoader().sourceAt(file).content(StandardCharsets.UTF_8));
     }
 
 }
