acb642b5ea372ee8d40d6349c632c954a0c8ca51
devnewton <devnewton@bci.im>
2019-09-30 16:09:39 +0200

    sse find post by id

--- src/main/java/im/bci/jb3/sse/SseCoin.java
+++ src/main/java/im/bci/jb3/sse/SseCoin.java
@@ -4,15 +4,18 @@ import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import im.bci.jb3.bouchot.data.Post;
+import im.bci.jb3.bouchot.data.PostRepository;
 import im.bci.jb3.bouchot.logic.UserPostHandler;
 import im.bci.jb3.event.NewPostsEvent;
 
@@ -23,22 +26,30 @@ public class SseCoin {
 	@Autowired
 	private UserPostHandler tribune;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	private CopyOnWriteArraySet<SseMoule> moules = new CopyOnWriteArraySet<>();
 
-	@GetMapping("/ssecoin")
-	public SseEmitter sse(@RequestParam(value = "nickname", required = false) String nickname) {
-		SseMoule moule = new SseMoule(nickname);
+	@GetMapping("/ssecoin/posts")
+	public SseEmitter posts() {
+		SseMoule moule = new SseMoule();
 		moules.add(moule);
 		moule.emiter.onCompletion(() -> moules.remove(moule));
 		return moule.emiter;
 	}
 
-	@PostMapping(value = "/ssecoin")
+	@PostMapping(value = "/ssecoin/posts")
 	public void post(@RequestParam(value = "nickname", required = false) String nickname,
 			@RequestParam(value = "message") String message,
 			@RequestParam(value = "room", required = false) String room,
 			@RequestParam(value = "auth", required = false) String auth) {
 		tribune.post(nickname, message, room, auth, ServletUriComponentsBuilder.fromCurrentRequest());
+	}
+
+	@GetMapping(path = "/ssecoin/posts/{messageId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Post findById(@PathVariable String messageId) {
+		return postRepository.findOne(messageId);
 	}
 
 	@EventListener
--- src/main/java/im/bci/jb3/sse/SseMoule.java
+++ src/main/java/im/bci/jb3/sse/SseMoule.java
@@ -4,10 +4,5 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public class SseMoule {
 	SseEmitter emiter = new SseEmitter(15L * 60L * 1000L);
-	String nickname;
-
-	public SseMoule(String nickname) {
-		this.nickname = nickname;
-	}
 
 }
